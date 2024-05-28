5d8de808a4ae48fbfd30d903bef89f0acc3e4ef0
monodo <olivier.monod@yverdon-les-bains.ch>
2024-05-28 11:33:13 +0200

    fix call to url that does not exist

--- geocity/apps/accounts/admin.py
+++ geocity/apps/accounts/admin.py
@@ -1011,13 +1011,11 @@ class TemplateCustomizationAdmin(admin.ModelAdmin):
 
     @admin.display(boolean=True)
     def has_background_image(self, obj):
-        try:
-            return obj.background_image.url is not None
-        except ValueError:
-            return False
+        return False if obj.background_image.name == "" else True
 
-    has_background_image.admin_order_field = "background_image"
-    has_background_image.short_description = "Image de fond"
+    if has_background_image:
+        has_background_image.admin_order_field = "background_image"
+        has_background_image.short_description = "Image de fond"
 
 
 # Inline for base Django Site
