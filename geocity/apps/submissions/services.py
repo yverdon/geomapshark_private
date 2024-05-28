a6ab80deb91be0a2fb9f96141ece38eb74737521
monodo <olivier.monod@yverdon-les-bains.ch>
2024-05-28 15:30:00 +0200

    force download (#1002)

--- geocity/apps/submissions/services.py
+++ geocity/apps/submissions/services.py
@@ -315,10 +315,10 @@ def login_for_anonymous_request(request, entity):
 
 def download_file(path):
     storage = fields.PrivateFileSystemStorage()
-    # for some strange reason, firefox refuses to download the file.
-    # so we need to set the `Content-Type` to `application/octet-stream` so
-    # firefox will download it. For the time being, this "dirty" hack works
-    return FileResponse(storage.open(path), content_type="application/octet-stream")
+    # Force all files to be downloaded and never opened in browser on same domain
+    return FileResponse(
+        storage.open(path), content_type="application/octet-stream", as_attachment=True
+    )
 
 
 def download_archives(archive_ids, user):
